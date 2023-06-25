96db80f0ba5a78a38ae7ae231c44066a8aad254f
Divyagoyal3 <Divyagoyal3>
2023-06-25 18:38:55 +0530

    completed'

--- text to emojii/script.js
+++ text to emojii/script.js
@@ -1,72 +1,120 @@
+// const text = document.querySelector("#textmsg")
+const password = document.querySelector('#password')
+const result = document.querySelector("#result")
 var clutter = "";
+var parinam = "";
 
 function encryption() {
-  document.querySelector("#encrypt-btn").addEventListener("click", function () {
-    var input = document.getElementById("txtmsg").value;
-    //getting input from user
-    console.log(input);
-    //gettinf password from user
-    var passw = document.getElementById("password").value;
-    console.log(passw);
-    //split the text
-    const str = input.split("");
-    console.log(str);
-    // converting it into emojii
-    str.forEach((element) => {
-      clutter += `&#128${element.charCodeAt()}`;
-    });
-    console.log(clutter);
-    // storing it into a result div
 
-    document.querySelector("#result").innerHTML = clutter;
+    document.querySelector("#encrypt-btn").addEventListener("click", function () {
 
-    var dataarr = [];
 
-    if (JSON.parse(localStorage.getItem("data1"))) {
-      dataarr = JSON.parse(localStorage.getItem('data1'))
-      dataarr.push({ pass: password, input: input, clutter: clutter });
-    } else {
-      dataarr = [{ pass: password, input: input, clutter: clutter }];
-    }
+        // get the password
 
-    localStorage.setItem("data1", JSON.stringify(dataarr));
-  });
+        var pass = document.getElementById("password").value;
+        console.log(pass)
+
+
+        // get the input
+
+        var input = document.getElementById("textmsg").value;
+        console.log(input)
+
+        //converting it into a set of emojis
+
+        var str = input.split("")
+        str.forEach(element => {
+            clutter += `&#128${(element.charCodeAt())} `
+            // console.log((element.charCodeAt()) * Math.floor(Math.random() * 10))
+        });
+        // console.log(clutter)
+
+        //saving emojis in localstorage
+
+        // localStorage.setItem("emojis", clutter)
+
+        //showing result
+
+        document.querySelector("#result").innerHTML = clutter
+
+        var dataarr = [];
+        if(JSON.parse(localStorage.getItem('data1'))){
+            dataarr = JSON.parse(localStorage.getItem('data1'));
+            console.log(dataarr)
+            dataarr.push({"pass":pass, "input":input, "clutter":clutter})
+        }else{
+            dataarr = [{"pass":pass,"input":input,"clutter":clutter}]
+        }
+        localStorage.setItem(`data1`, JSON.stringify(dataarr))
+    })
+
+}
+
+function decryption() {
+    document.querySelector("#decrypt-btn").addEventListener("click", function () {
+        var clutter2 = '';
+        var input2 = document.querySelector("#emojimsg").value
+        var finalPass = document.querySelector("#finalpassword").value
+        var user = JSON.parse(localStorage.getItem('data1'))
+        console.log(user)
+        var str2 = input2.split(" ")
+        str2.forEach(element => {
+                clutter2 += `&#${(element.codePointAt(0))} `
+                // console.log((element.charCodeAt()) * Math.floor(Math.random() * 10))
+        });
+        console.log(clutter2)
+        var found;
+        for(let i of user){
+            if(i.clutter == clutter2){
+                found = i;
+                console.log(i)
+            }
+        }
+        if (found.clutter === clutter2) {
+            console.log("jay ho")
+            document.querySelector("#result").style.display = `block`
+            document.querySelector("#result").style.color = `#eee`
+
+            document.querySelector("#result").innerHTML = found.input
+        } else {
+            document.querySelector("#result").style.display = `block`
+            document.querySelector("#result").style.color = `red`
+            document.querySelector("#result").innerHTML = "Wrong password!"
+        }
+    })
+
+}
+
+
+function btnClicking() {
+
+    document.querySelector("button").addEventListener("click", function () {
+        document.querySelector("#result").style.display = "block"
+        // console.log(localStorage.getItem("password"))
+        // console.log(localStorage.getItem("emojis"))
+    })
+    document.querySelector("#dec-btn").addEventListener("click", function () {
+        document.querySelector("#result").style.display = "none"
+        document.querySelector("#decryption").style.display = "block"
+        document.querySelector("#encryption").style.display = "none"
+        document.querySelector("#dec-btn").style.backgroundColor = "#333"
+        document.querySelector("#enc-btn").style.backgroundColor = "#222"
+        document.querySelector("#main>h1 span img").style.rotate = '270deg'
+    })
+    document.querySelector("#enc-btn").addEventListener("click", function () {
+        document.querySelector("#decryption").style.display = "none"
+        document.querySelector("#result").style.display = "none"
+        document.querySelector("#encryption").style.display = "block"
+        document.querySelector("#dec-btn").style.backgroundColor = "#222"
+        document.querySelector("#enc-btn").style.backgroundColor = "#333"
+        document.querySelector("#main>h1 span img").style.rotate = '90deg'
+
+    })
 }
 
 encryption();
 
-function btnclicking() {
-  document.querySelector("#dec-btn").addEventListener("click", function () {
-    document.querySelector("#decryption").style.display = "block";
-    document.querySelector("#encryption").style.display = "none";
-    document.querySelector("#dec-btn").style.backgroundColor = "#333";
-    document.querySelector("#enc-btn").style.backgroundColor = "#222";
-    document.querySelector("#result").style.display = "none";
-    document.querySelector("#main>h1 span img").style.rotate = "270deg";
-  });
-  document.querySelector("#enc-btn").addEventListener("click", function () {
-    document.querySelector("#encryption").style.display = "block";
-    document.querySelector("#decryption").style.display = "none";
-    document.querySelector("#enc-btn").style.backgroundColor = "#333";
-    document.querySelector("#dec-btn").style.backgroundColor = "#222";
-    document.querySelector("#main>h1 span img").style.rotate = "90deg";
-    document.querySelector("#result").style.display = "none";
-  });
-  document.querySelector("#encrypt-btn").addEventListener("click", function () {
-    document.querySelector("#result").style.display = "block";
-  });
+decryption()
 
-  document.querySelector("#decrypt-btn").addEventListener("click", function () {
-    document.querySelector("#result").style.display = "block";
-  });
-}
+btnClicking();
 
-btnclicking();
-
-// localStorage.setItem('username','divya')
-
-// var a = localStorage.getItem('username')
-// console.log(a);
-// The localStorage object allows you to save key/value pairs in the browser.
-// The localStorage object stores data with no expiration date.
-// The data is not deleted when the browser is closed, and are available for future sessions.
